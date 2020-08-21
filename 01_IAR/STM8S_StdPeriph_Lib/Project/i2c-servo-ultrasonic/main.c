322611da8f2e88427d290871f693d2f199818bb9
Phuong Le <leducphuong.hcm@gmail.com>
2020-08-21 18:10:11 +0700

    new iRQ when distance detect fix

--- 01_IAR/STM8S_StdPeriph_Lib/Project/i2c-servo-ultrasonic/main.c
+++ 01_IAR/STM8S_StdPeriph_Lib/Project/i2c-servo-ultrasonic/main.c
@@ -553,7 +553,7 @@ void init_tim2()
         uint8_t cm [2];// =0;// (stop-start)/58;
         cm[1]=0x00;cm[0]=0x00;
         cm[0] = (stop-start)/58;//58
-        _sr04_dis_reg = cm[0];
+        //_sr04_dis_reg = cm[0];
         // printf("%uus %ucm\r\n", (stop-start),(stop-start)/58);
         //TIM2->CR1 &= ~TIM2_CR1_CEN; // stop timer 2
         if(en_irq_distance==1 && (_sr04_dis_reg<=distance_detected) && cm[0]!=_sr04_dis_reg)//kiem tra co enable ngat khong co thì sinh ngat
